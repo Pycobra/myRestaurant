ad56c285c613a73b6c0b4f5d5f112f498c899610
Bright Orji <megabright007@gmail.com>
2023-09-22 13:51:29 +0100

    new changes are made

--- myRestaurant/settings/core_settings.py
+++ myRestaurant/settings/core_settings.py
@@ -162,8 +162,12 @@ WSGI_APPLICATION = 'myRestaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("PGDATABASE"),
+        'USER': os.getenv("PGUSER"),
+        "PASSWORD": os.getenv("PGPASSWORD"),
+        "HOST": os.getenv("PGHOST"),
+        "PORT": os.getenv("PGPORT"),
     }
 }
 
