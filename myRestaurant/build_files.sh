0b3dbfc3b4fe817cb96ae150e4b1375be405efb9
Bright Orji <megabright007@gmail.com>
2023-09-22 12:41:52 +0100

    new changes are made

---
+++ myRestaurant/build_files.sh
@@ -0,0 +1,4 @@
+pip intall -r requirements.txt
+
+python3.8 manage.py migrate
+python3.8 manage.py collectstatic
--- myRestaurant/wsgi.py
+++ myRestaurant/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myRestaurant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myRestaurant.settings.core_settings')
 
 application = get_wsgi_application()
